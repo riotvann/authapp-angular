b782c797413249913d94992203d9e1cf893a04f9
jgamesl <jgamezldev@gmail.com>
2024-09-06 09:17:42 -0700

    first commit

---
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -0,0 +1,39 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import Swal from 'sweetalert2'
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.css'
+})
+export class LoginPageComponent {
+
+
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  public myForm: FormGroup = this.fb.group({
+    email: ['cosa@gmail.com', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]],
+
+  });
+
+  login() {
+    const { email, password } = this.myForm.value;
+    this.authService.login(email, password)
+      .subscribe({
+        next: () => {
+          this.router.navigateByUrl('/dashboard')
+        },
+        error: (error) => {
+          Swal.fire('Error', error, 'error')
+          console.log({ loginError: error })
+        }
+      })
+  }
+
+}
---
+++ src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -0,0 +1,21 @@
+import { Component, computed, inject } from '@angular/core';
+import { AuthService } from '../../../auth/services/auth.service';
+import { AuthStatus } from '../../../auth/interfaces';
+
+@Component({
+  selector: 'app-dashboard-layout',
+  templateUrl: './dashboard-layout.component.html',
+  styleUrl: './dashboard-layout.component.css'
+})
+export class DashboardLayoutComponent {
+  private authService = inject(AuthService);
+  public user = computed(() => this.authService.currentUser());
+
+  // get user() {
+  //   return this.authService.currentUser();
+  // }
+
+  logout() {
+    this.authService.logout();
+  }
+}
